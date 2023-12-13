236d41e55ae47df8a5eeea76c5616479c4c674b8
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-13 20:32:48 +0530

    Resolve 101

--- modules/resource_group/variables.tf
+++ modules/resource_group/variables.tf
@@ -1,36 +1,36 @@
-variable "project_prefix" {
-  type        = string
-  description = "Project Prefix"
-}
+# variable "project_prefix" {
+#   type        = string
+#   description = "Project Prefix"
+# }
 
-variable "environment_prefix" {
-  description = "Environment Prefix (ex. DEV, STAGING, PROD)"
-  type        = string
-}
+# variable "environment_prefix" {
+#   description = "Environment Prefix (ex. DEV, STAGING, PROD)"
+#   type        = string
+# }
 
-variable "environment_version" {
-  type        = string
-  description = "Environment Version "
-}
+# variable "environment_version" {
+#   type        = string
+#   description = "Environment Version "
+# }
 
-variable "resource_group_prefix" {
-  type        = string
-  description = "Resource Group Prefix"
-}
+# variable "resource_group_prefix" {
+#   type        = string
+#   description = "Resource Group Prefix"
+# }
 
-variable "resource_group_version" {
-  type        = string
-  description = "Resource Group Version"
-}
+# variable "resource_group_version" {
+#   type        = string
+#   description = "Resource Group Version"
+# }
 
 variable "location" {
   type        = string
   description = "Resource Group Location"
 }
 
-variable "azure_provider" {
-  description = "Azure Provider Configuration"
-}
+# variable "azure_provider" {
+#   description = "Azure Provider Configuration"
+# }
 
 # data "azurerm_resource_group" "example" {
 #   provider = var.azure_provider
