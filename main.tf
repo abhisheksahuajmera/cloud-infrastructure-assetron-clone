afa34d3f38e3642f676ffd67af2471310706672c
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-13 15:23:23 +0530

    Try 200

--- main.tf
+++ main.tf
@@ -4,14 +4,9 @@ module "bootstrapping_resource_group" {
   location = var.resource_group_location
 }
 
-data "azurerm_resource_group" "azurerm_resource_grouptg" {
-  name     = module.bootstrapping_resource_group.nameop
-  location = module.bootstrapping_resource_group.locationop
-}
-
 module "securing_adb2c" {
   source                  = "./modules/adb2c" // Add version after registry
   country_code            = var.resource_group_location_country_code
   data_residency_location = var.resource_group_data_residency_location
-  resource_group_name     = data.azurerm_resource_group.azurerm_resource_grouptg.name
+  resource_group_name     = module.bootstrapping_resource_group.resource_group_name
 }
