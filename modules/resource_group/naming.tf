de4a81c00951160f06447c10fa2d4c2081b930ae
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-13 19:39:03 +0530

    Resolve 104

--- modules/resource_group/naming.tf
+++ modules/resource_group/naming.tf
@@ -1,16 +1,16 @@
-resource "azurecaf_naming_convention" "rg_name" {
-  prefix        = var.project_prefix
-  name          = "${var.environment_prefix}-${var.environment_version}"
-  resource_type = "rg"
-  postfix       = var.resource_group_version
-}
-
-# module "naming_convention" {
-#   source  = "Azure/caf-naming/azurerm"
-#   version = "x.x.x"  # Use the appropriate version
-
-#   conventions = {
-#     # Configure naming convention parameters here
-#   }
+# resource "azurecaf_naming_convention" "rg_name" {
+#   prefix        = var.project_prefix
+#   name          = "${var.environment_prefix}-${var.environment_version}"
+#   resource_type = "rg"
+#   postfix       = var.resource_group_version
 # }
+
+module "azurecaf_naming_convention" {
+  source  = "Azure/caf-naming/azurerm"
+  # version = "x.x.x"  # Use the appropriate version
+
+  # conventions = {
+  #   # Configure naming convention parameters here
+  # }
+}
 
