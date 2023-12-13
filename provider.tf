a3f026e63e6ec1a7f0308656bf615ae9294e6d69
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-13 18:50:10 +0530

    Resolve 200

--- provider.tf
+++ provider.tf
@@ -1,15 +1,10 @@
 terraform {
   required_providers {
-    azuredevops = {
-      source  = "microsoft/azuredevops"
-      version = ">= 0.1.0"
-    }
-
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.7.0"
     }
-    
+
     azurecaf = {
       source = "aztfmod/azurecaf"
       version = "1.2.11"
