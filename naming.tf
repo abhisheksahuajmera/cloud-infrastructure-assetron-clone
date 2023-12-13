80de0993f46d3d31c209a7f987de4c68f1e6b652
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-13 20:55:47 +0530

    Resolve 101

--- naming.tf
+++ naming.tf
@@ -8,10 +8,10 @@ resource "azurecaf_name" "rg_name" {
 }
 
 # securing_adb2c
-resource "azurecaf_name" "rg_name" {
-  name          = "${var.environment_prefix}-${var.environment_version}"
-  resource_type = "aadb2c"
-  prefixes      = ["${var.project_prefix}"]
-  suffixes      = ["${var.resource_group_version}"]
-  random_length = 0
-}
+# resource "azurecaf_name" "rg_name" {
+#   name          = "${var.environment_prefix}-${var.environment_version}"
+#   resource_type = "aadb2c"
+#   prefixes      = ["${var.project_prefix}"]
+#   suffixes      = ["${var.resource_group_version}"]
+#   random_length = 0
+# }
