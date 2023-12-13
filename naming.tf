bc5530cf1c4f829b33880934e5826ee796cfb462
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-13 20:54:46 +0530

    Resolve 100

--- naming.tf
+++ naming.tf
@@ -1,17 +1,17 @@
 # bootstrapping_resource_group
 resource "azurecaf_name" "rg_name" {
   name          = "${var.environment_prefix}-${var.environment_version}"
-  resource_type = "${var.resource_group_prefix}"
+  resource_type = "rg"
   prefixes      = ["${var.project_prefix}"]
   suffixes      = ["${var.resource_group_version}"]
   random_length = 0
 }
 
 # securing_adb2c
-# resource "azurecaf_name" "rg_name" {
-#   name          = "${var.environment_prefix}-${var.environment_version}"
-#   resource_type = "aadb2c"
-#   prefixes      = ["${var.project_prefix}"]
-#   suffixes      = ["${var.resource_group_version}"]
-#   random_length = 0
-# }
+resource "azurecaf_name" "rg_name" {
+  name          = "${var.environment_prefix}-${var.environment_version}"
+  resource_type = "aadb2c"
+  prefixes      = ["${var.project_prefix}"]
+  suffixes      = ["${var.resource_group_version}"]
+  random_length = 0
+}
