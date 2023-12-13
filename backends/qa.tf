180996983cd413bf74a55baaafe3a53c18b42972
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-13 15:01:46 +0530

    Resolve 100

--- backends/qa.tf
+++ backends/qa.tf
@@ -1,8 +1,4 @@
-terraform {
-    backend "azurerm" {
-    resource_group_name   = "Assetronai-qa-101-rg-108"
-    storage_account_name  = "sa110"
-    container_name        = "sacon100"
-    key                   = "state103.tfstate"
-  }  
-}
+resource_group_name  = "Assetronai-qa-101-rg-108"
+storage_account_name = "sa110"
+container_name       = "sacon100"
+key                  = "state103.tfstate"
