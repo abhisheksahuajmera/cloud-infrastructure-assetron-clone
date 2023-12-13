3e4d75c78f7fb88a2ef5bcb78ff9870eeb245210
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-13 19:00:02 +0530

    Resolve 400

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azurecaf = {
+      source = "aztfmod/azurecaf"
+      version = "1.2.10"
+    }
+  }
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = azurecaf_name.rg_name.result
   location = var.location
--- provider.tf
+++ provider.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.7.0"
     }
+
+    azurecaf = {
+      source = "aztfmod/azurecaf"
+      version = "1.2.10"
+    }
   }
 }
 
