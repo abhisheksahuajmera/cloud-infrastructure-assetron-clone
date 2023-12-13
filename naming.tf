f710902f53b116414e2f7a9a839e86b61c83b485
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-13 21:22:58 +0530

    Last Name

--- naming.tf
+++ naming.tf
@@ -1,21 +1,22 @@
 # bootstrapping_resource_group
 resource "azurecaf_name" "rg_name" {
   prefixes      = ["${var.project_prefix}"]
-  name          = "${var.environment_prefix}-${var.environment_version}"
+  name          = var.environment_prefix
   resource_type = "azurerm_resource_group"
-  suffixes      = ["${var.resource_group_version}"]
+  suffixes      = [local.resource_group_location_modified]
   random_length = 0
 }
 
 # securing_adb2c
 resource "azurecaf_name" "adb2c_name" {
-  name          = "${var.environment_prefix}-${var.environment_version}"
+  name          = var.environment_prefix
   resource_type = "azurerm_aadb2c_directory"
   prefixes      = ["${var.project_prefix}"]
-  suffixes      = ["${var.resource_group_version}"]
+  suffixes      = [local.resource_group_location_modified]
   random_length = 0
 }
 
 locals {
-  adb2c_name_modified = replace(replace(azurecaf_name.adb2c_name.result, "-", " "), "_", " ")
+  resource_group_location_modified = replace(var.resource_group_version, " ", "")
+  adb2c_name_modified              = replace(replace(azurecaf_name.adb2c_name.result, "-", " "), "_", " ")
 }
