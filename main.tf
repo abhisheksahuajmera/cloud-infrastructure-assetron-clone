14bcb696f941f2474eb543191647aae4d55ac830
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-14 10:04:07 +0530

    CICD Test 100

--- main.tf
+++ main.tf
@@ -11,3 +11,9 @@ module "securing_adb2c" {
   display_name            = local.adb2c_name_modified
   resource_group_name     = module.bootstrapping_resource_group.rg_name
 }
+
+module "networking_public_ip" {
+  source              = "./modules/public_ip"
+  name                = azurecaf_name.public_ip_name.result
+  resource_group_name = module.bootstrapping_resource_group.rg_name
+}
--- naming.tf
+++ naming.tf
@@ -16,6 +16,15 @@ resource "azurecaf_name" "adb2c_name" {
   random_length = 0
 }
 
+# networkin_public_ip
+resource "azurecaf_name" "public_ip_name" {
+  name          = var.environment_prefix
+  resource_type = "azurerm_public_ip"
+  prefixes      = ["${var.project_prefix}"]
+  suffixes      = [local.resource_group_location_modified]
+  random_length = 0
+}
+
 locals {
   resource_group_location_modified = replace(var.resource_group_location, " ", "")
   adb2c_name_modified              = replace(azurecaf_name.adb2c_name.result, "-", "")
