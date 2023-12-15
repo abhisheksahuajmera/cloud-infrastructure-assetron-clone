6a47534937f8ac301c2f1e94562e7d8396a2d48b
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-15 14:33:05 +0530

    Dev Feat-Push Try

--- provider.tf
+++ provider.tf
@@ -2,13 +2,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.7.0"
+      version = ">= 3.85.0"
     }
 
     azurecaf = {
       source  = "aztfmod/azurecaf"
-      version = ">= 1.2.10"
+      version = ">= 1.2.26"
     }
+
+    required_version = ">= 1.6.6"
   }
 }
 
